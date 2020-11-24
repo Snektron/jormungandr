1783ee512f5eb2ab624849cafce972173c4d6142
BIsTheAnswer <usebastheanswer@gmail.com>
2020-11-24 18:31:42 +0100

    Exception handling and file flush

--- src/main.cpp
+++ src/main.cpp
@@ -16,39 +16,47 @@
 using node_type = uint32_t;
 
 auto main(int argc, char* argv[]) -> int {
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <graph.tsv> <webgraph.out>" << std::endl;
+    try {
+        if (argc < 3) {
+            std::cerr << "Usage: " << argv[0] << " <graph.tsv> <webgraph.out>" << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        auto in = std::ifstream(argv[1]);
+        auto original = TsvDecoder<node_type>(in).decode();
+
+        original.for_each([](node_type src, std::span<const node_type> neighbors) {
+            std::cout << src << ": out_degree = " << neighbors.size() << std::endl;
+            for (const auto dst : neighbors) {
+                std::cout << src << " -> " << dst << std::endl;
+            }
+        });
+
+        std::cout << "-----------" << std::endl;
+
+        auto ss = std::stringstream();
+        auto encoding = EncodingConfig();
+        auto encoder = WebGraphEncoder<node_type>(ss, encoding, original);
+        encoder.encode();
+
+        auto file_out = std::ofstream(argv[2], std::ios::binary);
+        file_out << ss.rdbuf();
+        ss.seekg(0);
+        file_out.flush();
+        file_out.close();
+
+        auto decoder = WebGraphDecoder(ss, original.num_nodes(), encoding);
+        while (auto node = decoder.next_node()) {
+            std::cout << node.value().index << ": out_degree = " << node.value().neighbors.size() << std::endl;
+            for (const auto n : node.value().neighbors) {
+                std::cout << node.value().index << " -> " << n << std::endl;
+            }
+        }
+
+        return EXIT_SUCCESS;
+    }
+    catch(const std::runtime_error& err) {
+        std::cerr << "Exception occurred: " << err.what() << std::endl;
         return EXIT_FAILURE;
     }
-
-    auto in = std::ifstream(argv[1]);
-    auto original = TsvDecoder<node_type>(in).decode();
-
-    original.for_each([](node_type src, std::span<const node_type> neighbors) {
-        std::cout << src << ": out_degree = " << neighbors.size() << std::endl;
-        for (const auto dst : neighbors) {
-            std::cout << src << " -> " << dst << std::endl;
-        }
-    });
-
-    std::cout << "-----------" << std::endl;
-
-    auto ss = std::stringstream();
-    auto encoding = EncodingConfig();
-    auto encoder = WebGraphEncoder<node_type>(ss, encoding, original);
-    encoder.encode();
-
-    auto file_out = std::ofstream(argv[2], std::ios::binary);
-    file_out << ss.rdbuf();
-    ss.seekg(0);
-
-    auto decoder = WebGraphDecoder(ss, original.num_nodes(), encoding);
-    while (auto node = decoder.next_node()) {
-        std::cout << node.value().index << ": out_degree = " << node.value().neighbors.size() << std::endl;
-        for (const auto n : node.value().neighbors) {
-            std::cout << node.value().index << " -> " << n << std::endl;
-        }
-    }
-
-    return EXIT_SUCCESS;
 }
