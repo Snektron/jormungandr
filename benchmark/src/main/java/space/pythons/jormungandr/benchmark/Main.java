59c9485ea2f79ce35c6bc6a870ef6344629185e3
Robin Voetter <robin@voetter.nl>
2020-12-08 01:01:53 +0100

    java encode benchmark

--- benchmark/src/main/java/space/pythons/jormungandr/benchmark/Main.java
+++ benchmark/src/main/java/space/pythons/jormungandr/benchmark/Main.java
@@ -2,28 +2,51 @@ package space.pythons.jormungandr.benchmark;
 
 import it.unimi.dsi.webgraph.BVGraph;
 import it.unimi.dsi.webgraph.NodeIterator;
+import it.unimi.dsi.webgraph.ArrayListMutableGraph;
+import it.unimi.dsi.logging.ProgressLogger;
 
 public class Main {
     static String USAGE =
         "Usage: either of\n" +
-        "benchmark encode [--threads <n>] <input basename> <output basename>\n" +
-        "benchmark decode <input basename>";
+        "benchmark encode [encode options] <input basename> <output basename>\n" +
+        "benchmark decode <input basename>\n" +
+        "where [encode options] may consist of:\n" +
+        "--threads <int>\n" +
+        "--window-size <int>\n" +
+        "--zeta-k <int>\n" +
+        "--min-interval-size\n" +
+        "--max-ref-count";
 
     static void encode(String[] args) throws Throwable {
         int threads = 1;
+        int window_size = 7;
+        int zeta_k = 3;
+        int min_interval_size = 2;
+        int max_ref_count = 3;
+
         String in_basename = null;
         String out_basename = null;
 
         for (int i = 1; i < args.length; ++i) {
-            if (args[i].equalsIgnoreCase("--threads")) {
-                ++i;
-                if (i >= args.length) {
-                    System.err.println("Error: Missing argument after --threads");
-                    return;
-                }
+            switch (args[i]) {
+                case "--threads":
+                    threads = Integer.parseInt(args[i++], 10);
+                    continue;
+                case "--window-size":
+                    window_size = Integer.parseInt(args[i++], 10);
+                    continue;
+                case "--zeta-k":
+                    window_size = Integer.parseInt(args[i++], 10);
+                    continue;
+                case "--min-interval-size":
+                    window_size = Integer.parseInt(args[i++], 10);
+                    continue;
+                case "--max-ref-count":
+                    window_size = Integer.parseInt(args[i++], 10);
+                    continue;
+            }
 
-                threads = Integer.parseInt(args[i], 10);
-            } else if (in_basename == null) {
+            if (in_basename == null) {
                 in_basename = args[i];
             } else if (out_basename == null) {
                 out_basename = args[i];
@@ -37,6 +60,24 @@ public class Main {
             System.err.println(USAGE);
             return;
         }
+
+        var graph = new ArrayListMutableGraph(BVGraph.load(in_basename, 0));
+
+        long start = System.nanoTime();
+        BVGraph.store(
+            graph.immutableView(),
+            out_basename,
+            window_size,
+            max_ref_count,
+            min_interval_size,
+            zeta_k,
+            BVGraph.OUTDEGREES_DELTA | BVGraph.REFERENCES_UNARY | BVGraph.RESIDUALS_ZETA,
+            threads,
+            new ProgressLogger()
+        );
+        long stop = System.nanoTime();
+
+        System.out.println("" + (stop - start));
     }
 
     static void decode(String[] args) throws Throwable {
